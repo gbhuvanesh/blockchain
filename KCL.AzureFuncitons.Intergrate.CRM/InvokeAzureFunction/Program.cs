1d7e06e3f454a76f0eb5f7cae77e25e1f6d7092c
Gopalan Bhuvanesh <gopalanbhuvanesh@outlook.com>
2021-12-27 19:34:33 +0000

    console app client

---
+++ KCL.AzureFuncitons.Intergrate.CRM/InvokeAzureFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvokeAzureFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientCredential = new ClientCredential("{app_client_id}", "{app_client_secret}");
+            AuthenticationContext context = new AuthenticationContext("https://login.microsoftonline.com/xx.onmicrosoft.com", false);
+            AuthenticationResult authResult = context.AcquireTokenAsync(
+                "{app_client_id}",
+                clientCredential).Result;
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            var response = client.GetAsync("https://tonytestad.azurewebsites.net/api/HttpTrigger1?code=LoyOi4C&name=123").Result;
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
