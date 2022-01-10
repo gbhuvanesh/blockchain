8be6aff639862b5cbb8903d9b238052534c03a38
Gopalan Bhuvanesh <gopalanbhuvanesh@outlook.com>
2022-01-10 20:06:56 +0000

    update

--- KCL.AzureFuncitons.Intergrate.CRM/InvokeAzureFunction/Program.cs
+++ KCL.AzureFuncitons.Intergrate.CRM/InvokeAzureFunction/Program.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Extensions.Msal;
 
 namespace InvokeAzureFunction
 {
@@ -12,10 +13,38 @@ namespace InvokeAzureFunction
     {
         static void Main(string[] args)
         {
-            var clientCredential = new ClientCredential("{app_client_id}", "{app_client_secret}");
-            AuthenticationContext context = new AuthenticationContext("https://login.microsoftonline.com/xx.onmicrosoft.com", false);
+            // 1. Configuration - read below about redirect URI
+            var pca = PublicClientApplicationBuilder.Create("client_id")
+            .WithBroker()
+            .Build();
+            // Add a token cache, see https://docs.microsoft.com/en-us/azure/active-directory/develop/msal-net-tokencache-
+            serialization? tabs = desktop
+// 2. GetAccounts
+var accounts = await pca.GetAccountsAsync();
+            var accountToLogin = // choose an account, or null, or use PublicClientApplication.OperatingSystemAccount
+for the default OS account
+try
+                {
+                    // 3. AcquireTokenSilent
+                    var authResult = await pca.AcquireTokenSilent(new[] { "User.Read" }, accountToLogin)
+                    .ExecuteAsync();
+                }
+                catch (MsalUiRequiredException) // no change in the pattern
+                {
+                    // 4. Specific: Switch to the UI thread for next call . Not required for console apps.
+                    await SwitchToUiThreadAsync(); // not actual code, this is different on each platform / tech
+                                                   // 5. AcquireTokenInteractive
+                    var authResult = await pca.AcquireTokenInteractive(new[] { "User.Read" })
+                    .WithAccount(accountToLogin) // this already exists in MSAL, but it is more important for WAM
+                    .WithParentActivityOrWindow(myWindowHandle) // to be able to parent WAM's windows to your app (optional, but
+highly recommended; not needed on UWP)
+.ExecuteAsync();
+        }
+
+        var clientCredential = new ClientCredential("bad0fff4-629c-47e1-9abd-359db29e35c0", "m257Q~FU0t3jh8NLAcjlPqhfpguJ4It1BrMBD");
+            AuthenticationContext context = new AuthenticationContext("https://login.microsoftonline.com/b755419a-8313-4815-baf9-a96e7ef5b969/oauth2/v2.0/authorize", false);
             AuthenticationResult authResult = context.AcquireTokenAsync(
-                "{app_client_id}",
+                "bad0fff4-629c-47e1-9abd-359db29e35c0",
                 clientCredential).Result;
 
             HttpClient client = new HttpClient();
